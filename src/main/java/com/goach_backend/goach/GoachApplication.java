4bed29b9c7a3921eb05d8eaa0037158a3bdfb844
Ignacio Zelada <109553320+IgnacioZAraya@users.noreply.github.com>
2025-09-08 20:47:31 -0600

    Back End Setup

---
+++ src/main/java/com/goach_backend/goach/GoachApplication.java
@@ -0,0 +1,13 @@
+package com.goach_backend.goach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoachApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goach_backend/goach/GoachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goach_backend.goach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
