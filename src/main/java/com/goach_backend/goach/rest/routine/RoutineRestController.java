c089001dbcd712089512282c675cc08429752193
Ignacio Zelada <109553320+IgnacioZAraya@users.noreply.github.com>
2025-12-04 00:08:24 -0600

    - User Link Fix

--- src/main/java/com/goach_backend/goach/rest/routine/RoutineRestController.java
+++ src/main/java/com/goach_backend/goach/rest/routine/RoutineRestController.java
@@ -59,6 +59,7 @@ public class RoutineRestController {
 
     @GetMapping("/filterByUser/{userId}")
     public ResponseEntity<?> getRoutineByUserId(@PathVariable UUID userId) {
+
         List<Routine> r = routineRepository.findRoutineByTrainerId(userId);
 
         return ResponseEntity.ok(r);
